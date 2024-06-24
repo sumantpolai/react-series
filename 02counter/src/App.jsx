9b512f4fa06e8800d5988f65802977c387fa023b
sumantpolai <137427323+sumantpolai@users.noreply.github.com>
2024-06-24 03:33:28 +0000

    states

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -3,12 +3,18 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [counter, setCounter] = useState(15)   // here usestate is a type of method  and counter is a variable and setcounter is a function or method
   
-  let counter = 15
+  //let counter = 15
 
   const addValue = () =>{
-    counter + 1
-    console.log(counter);
+   setCounter(counter + 1)        // here we call the setcounter method
+
+
+  }
+  const removeValue = () =>{
+    setCounter(counter - 1)
   }
 
   return (
@@ -16,9 +22,11 @@ function App() {
       <h1>React course with hites {counter}</h1>  {/*here we inject the variable */}
       <h2>Counter value</h2>
        <button
-       onClick={addValue}  
+       onClick={addValue}     // after clicking addvalue it call the addvalue ehere we store a function and there is a setcounter is there who add 1 onclick
        >add value</button>{" "}   {/* it take space b/w two button  */}
-      <button>remove value</button>
+      <button
+      onClick={{removeValue}}
+      >remove value {counter}</button>
       <p>footer: {counter}</p>
       
     </>
