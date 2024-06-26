be38f369c12864418ee95040a5ca78d274d703fb
sumantpolai <137427323+sumantpolai@users.noreply.github.com>
2024-06-26 01:33:39 +0000

    bg changer project

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -4,17 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState('olive')
 
   return (
-   <div className='w-full h-screen duration-200'>
-    <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-      <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-        <button className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'>test</button>
-        <button className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'>test 2</button>
+    <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+          <button onClick={() => setColor('red')}
+            className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+            style={{ backgroundColor: 'red' }}
+          >test</button>
+          <button onClick={() => setColor('green')}
+            className='outline-none px-4 py-1 rounded-full  shadow-lg text-black'
+            style={{ backgroundColor: 'green' }}
+          >test 2</button>
+        </div>
       </div>
     </div>
-   </div>
   )
 }
 
